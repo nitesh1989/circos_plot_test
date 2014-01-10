ad5b5e524a1c76d84316d55a04fb008736d4018a
nitesh1989 <nturaga@andrew.cmu.edu>
2014-01-10 15:20:34 -0500

    Aclust analysis left to be done

--- circos.plots.biswal.R
+++ circos.plots.biswal.R
@@ -69,8 +69,8 @@ cyto.info = UCSC.HG19.Human.CytoBandIdeogram;
 # chr.exclude = c("chrX","chrY");
 chr.exclude = NULL;
 # Determine tracks inside and outside
-tracks.inside = 5;
-tracks.outside = 5;
+tracks.inside = 4;
+tracks.outside = 6;
 
 # Initialize RCircos Core components
 RCircos.Set.Core.Components(cyto.info, chr.exclude=chr.exclude,tracks.inside=tracks.inside,tracks.outside= tracks.outside);
@@ -108,7 +108,7 @@ params = RCircos.Get.Plot.Parameters()
 # params$text.size = 0.6;
 # 
 
-params$radius.len = 2.50;
+params$radius.len = 2.0;
 params$base.per.unit =1000;
 params$track.padding = 0.02;
 params$track.height = 0.2;
@@ -126,25 +126,25 @@ RCircos.List.Parameters() # For reference in Rout file
 # Step 4: Make RCircos plots
 ################################################
 
-out.file = "BiswalCircos4.png"
-png(file = out.file, height = 25, width = 25,res=750,units = "in")
+out.file = "BiswalCircos5.png"
+png(file = out.file, height = 30, width = 30,res=600,units = "in")
 #plot(main="Inner Tracks: H460 Knocking-Genes; Outer Tracks: H460 Parent-genes")
 RCircos.Set.Plot.Area()
 # Plot chromosome ideogram
 RCircos.Chromosome.Ideogram.Plot();
 
 # Information for track 1 "inside" - Plot gene names(column 13)
-RCircos.Gene.Name.Plot(gene.data= knock.x,name.col= 13,track.num=2,side="in")
-RCircos.Gene.Connector.Plot(genomic.data=knock.x,track.num=1,side = "in")
+RCircos.Gene.Name.Plot(gene.data= knock.x,name.col= 13,track.num=1,side="out")
+RCircos.Gene.Connector.Plot(genomic.data=knock.x,track.num=2,side = "out")
 
 #Information for track 1 "outside" - Plot gene names(column 15)
-RCircos.Gene.Name.Plot(gene.data=parent.x,name.col= 13,track.num=2,side="out")
-RCircos.Gene.Connector.Plot(genomic.data=parent.x,track.num=1,side="out")
+RCircos.Gene.Name.Plot(gene.data=parent.x,name.col= 13,track.num=2,side="in")
+RCircos.Gene.Connector.Plot(genomic.data=parent.x,track.num=1,side="in")
 
 # Plot Peaks like RCircos Line data
 
-RCircos.Line.Plot(line.data=knock.x,data.col=4,track.num=3,side="in")
-RCircos.Line.Plot(line.data=parent.x,data.col=4,track.num=3,side = "out")
+RCircos.Line.Plot(line.data=knock.x,data.col=4,track.num=3,side="out")
+RCircos.Line.Plot(line.data=parent.x,data.col=4,track.num=3,side = "in")
 
 ################################################
 # Step 5: Plot Pathways
@@ -152,11 +152,11 @@ RCircos.Line.Plot(line.data=parent.x,data.col=4,track.num=3,side = "out")
 
 # Plot in R Circos
 
-RCircos.Gene.Name.Plot(gene.data= knock_df,name.col= which(colnames(knock_df) == "pathwayGene"),track.num=5,side="in")
-RCircos.Gene.Connector.Plot(genomic.data=knock_df,track.num=4,side = "in")
+RCircos.Gene.Name.Plot(gene.data= knock_df,name.col= which(colnames(knock_df) == "pathwayGene"),track.num=4,side="out")
+# RCircos.Gene.Connector.Plot(genomic.data=knock_df,track.num=4,side = "out")
 
-RCircos.Gene.Name.Plot(gene.data= parent_df,name.col= which(colnames(parent_df) == "pathwayGene"),track.num=5,side="out")
-RCircos.Gene.Connector.Plot(genomic.data=parent_df,track.num=4,side = "out")
+RCircos.Gene.Name.Plot(gene.data= parent_df,name.col= which(colnames(parent_df) == "pathwayGene"),track.num=4,side="in")
+# RCircos.Gene.Connector.Plot(genomic.data=parent_df,track.num=4,side = "in")
 
 
 
